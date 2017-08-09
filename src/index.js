7b78503cd98c650ee7d36719c8933a594d595f36
Pancho Cofre <panchocofrest@gmail.com>
2017-08-09 12:17:10 -0400

    orden data setting

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ var source = {
         { key: 'Remove', position: 1, start: false, header: { visible: false, label: null, class: null, width: null }, row: { class: null, align: null, width: null, type: null } },
         { key: 'Cuenta', position: 2, start: true, header: { visible: true, label: 'Cuenta', class: 'num-label-del', width: 100 }, row: { class: 'num-label-del normal no-border', align: 'left', width: 100, type: 'String' } },
         { key: 'Tipo', position: 3, start: false, header: { visible: false, label: null, class: null, width: null }, row: { class: 'label collapsed', align: null, width: null, type: null } },
-        { tipoControl: 'Input', key: 'Glosa', position: 4, start: false, header: { visible: true, label: 'Glosa', class: null, width: 15 }, row: { class: 'normal', align: 'left', width: 15, type: 'String' } },
-        { tipoControl: 'Dropdown', key: 'CentroCostos', position: 5, start: false, header: { visible: true, label: 'Centro de Costos', class: null, width: 20 }, row: { class: 'normal', align: 'left', width: 20, type: 'String' } },
-        { tipoControl: 'Dropdown', key: 'Sucursal', position: 6, start: false, header: { visible: true, label: 'Sucursal', class: null, width: 10 }, row: { class: 'normal', align: 'left', width: 10, type: 'String' } },
+        { key: 'Glosa', tipoControl: 'Input', position: 4, start: false, header: { visible: true, label: 'Glosa', class: null, width: 15 }, row: { class: 'normal', align: 'left', width: 15, type: 'String' } },
+        { key: 'CentroCostos', tipoControl: 'Dropdown', position: 5, start: false, header: { visible: true, label: 'Centro de Costos', class: null, width: 20 }, row: { class: 'normal', align: 'left', width: 20, type: 'String' } },
+        { key: 'Sucursal', tipoControl: 'Dropdown', position: 6, start: false, header: { visible: true, label: 'Sucursal', class: null, width: 10 }, row: { class: 'normal', align: 'left', width: 10, type: 'String' } },
         { key: 'Debe', position: 7, start: false, header: { visible: true, label: '$ Debe', class: null, width: 10 }, row: { class: 'normal', align: 'right', width: 10, type: 'Int' } },
         { key: 'Haber', position: 8, start: false, header: { visible: true, label: '$ Haber', class: null, width: 10 }, row: { class: 'normal', align: 'right', width: 10, type: 'Int' } }
     ],
@@ -20,11 +20,11 @@ var source = {
         aux: {
             columns: [
                 { key: 'Remove', position: 0, start: false, row: { class: 'borrar', align: null, width: null, type: null } },
-                { tipoControl: 'Autocomplete', key: 'Cliente', position: 1, start: true, row: { class: 'num-del normal', align: 'left', width: 100, type: null } },
-                { tipoControl: 'Dropdown', key: 'Documento', position: 2, start: false, row: { class: 'normal', align: 'left', width: 10, type: null } },
+                { key: 'Cliente', tipoControl: 'Autocomplete', position: 1, start: true, row: { class: 'num-del normal', align: 'left', width: 100, type: null } },
+                { key: 'Documento', tipoControl: 'Dropdown', position: 2, start: false, row: { class: 'normal', align: 'left', width: 10, type: null } },
                 { key: 'Numero', position: 3, start: false, row: { class: 'normal', align: 'left', width: 5, type: null } },
-                { tipoControl: 'DateTimeInput', key: 'Fecha', position: 4, start: false, row: { class: 'normal', align: 'center', width: 15, type: null } },
-                { tipoControl: 'DateTimeInput', key: 'FechaInicio', position: 5, start: false, row: { class: 'normal', align: 'center', width: 15, type: null } },
+                { key: 'Fecha', tipoControl: 'DateTimeInput', position: 4, start: false, row: { class: 'normal', align: 'center', width: 15, type: null } },
+                { key: 'FechaInicio', tipoControl: 'DateTimeInput', position: 5, start: false, row: { class: 'normal', align: 'center', width: 15, type: null } },
                 { key: 'Debe', position: 6, start: false, row: { class: 'normal', align: 'right', width: 10, type: null } },
                 { key: 'Haber', position: 7, start: false, row: { class: 'normal', align: 'right', width: 10, type: null } },
                 { key: 'Saldo', position: 8, start: false, row: { class: 'normal', align: 'right', width: 10, type: null } }
@@ -34,10 +34,10 @@ var source = {
         ban: {
             columns: [
                 { key: 'Remove', position: 0, start: false, row: { class: 'borrar', align: null, width: null, type: null } },
-                { tipoControl: 'Autocomplete', key: 'Cliente', position: 1, start: true, row: { class: 'num-del normal', align: 'left', width: 100, type: null } },
-                { tipoControl: 'Dropdown', key: 'Documento', position: 2, start: false, row: { class: 'normal', align: 'left', width: 20, type: null } },
+                { key: 'Cliente', tipoControl: 'Autocomplete', position: 1, start: true, row: { class: 'num-del normal', align: 'left', width: 100, type: null } },
+                { key: 'Documento', tipoControl: 'Dropdown', position: 2, start: false, row: { class: 'normal', align: 'left', width: 20, type: null } },
                 { key: 'Numero', position: 3, start: false, row: { class: 'normal', align: 'left', width: 5, type: null } },
-                { tipoControl: 'DateTimeInput', key: 'Fecha', position: 4, start: false, row: { class: 'normal', align: 'center', width: 15, type: null } },
+                { key: 'Fecha', tipoControl: 'DateTimeInput', position: 4, start: false, row: { class: 'normal', align: 'center', width: 15, type: null } },
                 { key: 'Debe', position: 5, start: false, row: { class: 'normal', align: 'right', width: 10, type: null } },
                 { key: 'Haber', position: 6, start: false, row: { class: 'normal', align: 'right', width: 10, type: null } }
             ],
